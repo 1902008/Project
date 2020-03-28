fa6674b56892fb969f9e9da2e2d7362f7ddbaf07
1902008 <61820553+1902008@users.noreply.github.com>
2020-03-28 23:14:20 +0800

    Update Schedule.h

--- Schedule.h
+++ Schedule.h
@@ -1,45 +1,24 @@
 #ifndef SCHEDULE_H
 #define SCHEDULE_H
+
 #pragma once
+#include <vector>
 #include <string>
-
-using namespace std;
-
-class Schedule
-{   
-	//Private Variables  
-	private:
-		char destinations;
-		string planByDeadline;
-		string planByMinimumFDR;
-
-	//Public Methods 
-	public:
-
-		//Methods for displaying stuffs relevant to main menu call
-		void showAllFDR();
-		void showAllBento();
-		void showFDRCapacity();
-		void DisplayFDRSort();
-		void DisplayBentoSort();
-		void showBentoDeadline(int);
-		void showBentoAssignment(string);
-
-		//Methods for sorting Bento into FDR
-		void scheduleBentoByDeadline(string, char, int, string, char, int);
-		void scheduleBentoByMinimumFDRs(string, char, int, string, char, int);
-
-		//Method for writing & saving Schedule plan back to excel 
-		void writeSaveSchedulingPlan();
-		
-		//Method for displaying empty fdr
-		void showEmptyFDR(int);
-
-		//Method to call FDR's add delete and edit functions
-		void AddDeleteEditFDR();
-		void AddDeleteEditBento();
-
+#include "FDR.h"
+#include "Bento.h"
+class Schedule {
+private:
+	vector<FDR> fdr; //fdr object
+	vector<Bento> bento; //bento object
+	vector <string> scheduleAll;
+public:
+	Schedule(vector<FDR>, vector<Bento>);
+	void startSchedulingDeadline();
+	void displayFinalPlanByDeadline();
+	void displayUnfullFDR();
+	void resetVectorsandFlags();
+	void startSchedulingByMinFDRs();
+	void displayFinalPlanByMinimumFDR();
+	
 };
-
-#endif 
-
+#endif
